30324d0372ecea3ca85dc6e1d84cb4beda9bbfd9
Jack.Z.Tang <jack.z.tang@newegg.com>
2017-05-17 15:26:56 +0800

    init Brute Force

--- src/main/java/name/jacktang/matchingalgorithms/BruteForce.java
+++ src/main/java/name/jacktang/matchingalgorithms/BruteForce.java
@@ -6,4 +6,24 @@ package name.jacktang.matchingalgorithms;
  * @company Newegg Tech (China) Co, Ltd
  */
 public class BruteForce {
+
+    public int match(String source, String pattern) {
+        boolean match = false;
+        int index = -1;
+        for (int i = 0; i < source.length(); i++) {
+            for (int j = 0; j < pattern.length(); j++) {
+                if (source.charAt(i + j) != pattern.charAt(j)) {
+                    break;
+                }
+                if (j == pattern.length() - 1) {
+                    match = true;
+                    index = i;
+                }
+            }
+            if (match) {
+                break;
+            }
+        }
+        return index;
+    }
 }
---
+++ src/test/java/name/jacktang/matchingalgorithms/test/TestMatching.java
@@ -0,0 +1,21 @@
+package name.jacktang.matchingalgorithms.test;
+
+import name.jacktang.matchingalgorithms.BruteForce;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Jack.Z.Tang
+ * @date 2017/05/17
+ * @company Newegg Tech (China) Co, Ltd
+ */
+public class TestMatching {
+    private final static String source = "BBC ABCDABABCDABCDABDE";
+    private final static String pattern = "ABCDABD";
+
+    @Test
+    public void testBruteForce() {
+        BruteForce bruteForce = new BruteForce();
+        Assert.assertTrue(bruteForce.match(source, pattern) == 14);
+    }
+}
